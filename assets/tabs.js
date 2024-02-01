1b12ca622dd82c1704a6193996de8f23eae63d67
Berkay Çongal <berkaycongal@hotmail.com>
2024-02-02 00:06:35 +0300

    uzay sitesi

---
+++ assets/tabs.js
@@ -0,0 +1,9 @@
+const tabList = document.querySelector(`[role="tablist"]`);
+const tabs = tabList.querySelectorAll(`[role="tab"]`);
+
+
+
+tabLists.addEventListener("keydown", (e) => {
+    const keydownLeft =37;
+    const keydownRight = 39;
+})
